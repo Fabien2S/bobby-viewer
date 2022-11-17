7d7ad2ef1bf22ec7eada33a97263b2a8beee6621
Fabien2S <18100602+Fabien2S@users.noreply.github.com>
2022-11-17 17:18:13 +0100

    Replace nbt tooltip with info command

--- src/main/java/dev/fabien2s/bobby/BobbyMod.java
+++ src/main/java/dev/fabien2s/bobby/BobbyMod.java
@@ -1,7 +1,9 @@
 package dev.fabien2s.bobby;
 
+import dev.fabien2s.bobby.command.InfoCommand;
 import dev.fabien2s.bobby.util.EntityRendererPredicate;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
@@ -20,6 +22,10 @@ public class BobbyMod implements ModInitializer {
                 KeyMapping.CATEGORY_MISC,
                 () -> true
         ));
+
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            InfoCommand.register(dispatcher);
+        });
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             EntityRendererPredicate.skipCustomRenderingInput = toggleViewModel.isDown();
