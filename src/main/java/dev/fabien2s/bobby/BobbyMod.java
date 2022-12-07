37165ff90d51b0ffd37a359192d92048e489400f
Fabien2S <18100602+Fabien2S@users.noreply.github.com>
2022-12-07 20:33:35 +0100

    Remove InfoCommand

--- src/main/java/dev/fabien2s/bobby/BobbyMod.java
+++ src/main/java/dev/fabien2s/bobby/BobbyMod.java
@@ -1,9 +1,7 @@
 package dev.fabien2s.bobby;
 
-import dev.fabien2s.bobby.command.InfoCommand;
 import dev.fabien2s.bobby.util.EntityRendererPredicate;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
@@ -16,19 +14,15 @@ public class BobbyMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        toggleViewModel = KeyBindingHelper.registerKeyBinding(new ToggleKeyMapping(
+        this.toggleViewModel = KeyBindingHelper.registerKeyBinding(new ToggleKeyMapping(
                 "key.bobby.toggle_view_model",
                 GLFW.GLFW_KEY_K,
                 KeyMapping.CATEGORY_MISC,
                 () -> true
         ));
 
-        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
-            InfoCommand.register(dispatcher);
-        });
-
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            EntityRendererPredicate.skipCustomRenderingInput = toggleViewModel.isDown();
+            EntityRendererPredicate.skipCustomRenderingInput = this.toggleViewModel.isDown();
         });
     }
 
