08ab869ff89044542c7ef753d180b9766b8e2f24
Fabien2S <18100602+Fabien2S@users.noreply.github.com>
2022-12-16 23:10:17 +0100

    Fix custom head rendering in first person

---
+++ src/main/java/dev/fabien2s/bobby/mixin/renderer/layer/CustomHeadLayerMixin.java
@@ -0,0 +1,26 @@
+package dev.fabien2s.bobby.mixin.renderer.layer;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import dev.fabien2s.bobby.util.EntityRendererPredicate;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.layers.CustomHeadLayer;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CustomHeadLayer.class)
+public class CustomHeadLayerMixin<T extends LivingEntity, M extends EntityModel<T>> {
+
+    @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V", at = @At("HEAD"), cancellable = true)
+    private void render(PoseStack poseStack, MultiBufferSource multiBufferSource, int i, T livingEntity, float f, float g, float h, float j, float k, float l, CallbackInfo ci) {
+        if (EntityRendererPredicate.skipCustomRendering(livingEntity)) {
+            return;
+        }
+
+        ci.cancel();
+    }
+
+}
